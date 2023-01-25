95fd263e315a1af8293e1980391fbc79101bcee7
Phaniraj <79626160+phani-training@users.noreply.github.com>
2023-01-25 16:54:59 +0530

    Add files via upload

---
+++ Dotnet Programming/CompleteDotnetTraining/DataAccessLib/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLib
+{
+    public interface IProductDB
+    {
+        void AddProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(int id);
+        List<Product> GetAllProducts();
+    }
+
+    public static class ProductFactory
+    {
+        public static IProductDB GetComponent() => new ProductDB();
+    }
+    class ProductDB : IProductDB
+    {
+        static ProductEntities context = new ProductEntities();
+        public void AddProduct(Product product)
+        {
+            context.Products.Add(product);
+            context.SaveChanges();
+        }
+        public void DeleteProduct(int id)
+        {
+            var found = context.Products.First((p) => p.ProductId == id);
+            context.Products.Remove(found);
+            context.SaveChanges();
+        }
+
+        public List<Product> GetAllProducts() => context.Products.ToList();
+
+        public void UpdateProduct(Product product)
+        {
+            var found = context.Products.First((p) => p.ProductId == product.ProductId);
+            found.ProductImage = product.ProductImage;
+            found.ProductName = product.ProductName;
+            found.ProductPrice = product.ProductPrice;
+            found.Quantity = product.Quantity;
+            context.SaveChanges();
+        }
+    }
+}
